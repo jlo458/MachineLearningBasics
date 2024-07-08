f3980bfe2eb0e76cb77d213da2a8749e1aaf1bb7
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-07-08 17:05:57 +0100

    Update simpleHousePrices.py

--- simpleHousePrices.py
+++ simpleHousePrices.py
@@ -1,10 +1,9 @@
 # Unfinished code for line of regression 
 # Haven't figured out matplotlib yet
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import random
-import utils  
 
 def squTrick(basePrice, roomPrice, numRooms, price, learningRate): 
     predictPrice = roomPrice*numRooms + basePrice  # y = mx + c 
@@ -17,24 +16,35 @@ def linearRegression(features, labels, learningRate=0.01, iterations=1000):
     roomPrice = random.random()
     for iteration in range(iterations): 
 
-        if True: 
-            utils.draw_line(roomPrice, basePrice, starting=0, ending=0)
+        labelVals = np.empty(len(numberOfRooms), dtype=int)
+        #print(labelVals)
+        for ind in range(len(numberOfRooms)): 
+            labelVal = roomPrice*(numberOfRooms[ind]) + basePrice 
+            labelVals[ind] = labelVal
+            
+
+
+        #print(f"{numberOfRooms} and then -- {labelVals}")
+        #plt.plot(numberOfRooms, labelVals)
+        #plt.show()
 
         index = random.randint(1, len(features)-1)
         numRooms = numberOfRooms[index]
         price = housePrices[index]
         squTrick(basePrice, roomPrice, numRooms, price, learningRate) 
 
-    utils.draw_line(roomPrice, basePrice, "black", starting=0, ending=8)
-    utils.plot_points(features, labels)
+  
     print(f"Price per room {roomPrice}")
     print(f"Base price {basePrice}")
     return roomPrice, basePrice
 
 
 
-plt.ylim(0,500)
+#plt.ylim(0,500)
 
 numberOfRooms = np.array([1,2,3,5,6,7])
 housePrices = np.array([155,197,244,356,407,448])
-linearRegression(numberOfRooms, housePrices)
+x, y = linearRegression(numberOfRooms, housePrices)
+
+plt.plot(x,y,"r")
+plt.show()
