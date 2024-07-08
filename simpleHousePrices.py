aea5fcfd21d953b98c1b729a67d2c87256da5d0b
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-07-08 18:13:31 +0100

    Update simpleHousePrices.py

--- simpleHousePrices.py
+++ simpleHousePrices.py
@@ -1,5 +1,5 @@
-# Unfinished code for line of regression 
-# Haven't figured out matplotlib yet
+# First ml program 
+# Calculates line of regression using "square trick"
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,31 +7,20 @@ import random
 
 def squTrick(basePrice, roomPrice, numRooms, price, learningRate): 
     predictPrice = roomPrice*numRooms + basePrice  # y = mx + c 
-    basePrice += (price-predictPrice) + learningRate
+    basePrice += (price-predictPrice)*learningRate 
     roomPrice += (price-predictPrice)*numRooms*learningRate 
     return roomPrice, basePrice 
 
-def linearRegression(features, labels, learningRate=0.01, iterations=1000):
+def linearRegression(features, labels, learningRate=0.01, iterations=5000):
     basePrice = random.random()
     roomPrice = random.random()
-    for iteration in range(iterations): 
-
-        labelVals = np.empty(len(numberOfRooms), dtype=int)
-        #print(labelVals)
-        for ind in range(len(numberOfRooms)): 
-            labelVal = roomPrice*(numberOfRooms[ind]) + basePrice 
-            labelVals[ind] = labelVal
-            
-
-
-        #print(f"{numberOfRooms} and then -- {labelVals}")
-        #plt.plot(numberOfRooms, labelVals)
-        #plt.show()
-
+    for _ in range(iterations): 
         index = random.randint(1, len(features)-1)
-        numRooms = numberOfRooms[index]
-        price = housePrices[index]
-        squTrick(basePrice, roomPrice, numRooms, price, learningRate) 
+        numRooms = features[index]
+        price = labels[index] 
+        
+        roomPrice, basePrice = squTrick(basePrice, roomPrice, numRooms, price, learningRate) 
+        print(roomPrice, basePrice)
 
   
     print(f"Price per room {roomPrice}")
@@ -39,12 +28,19 @@ def linearRegression(features, labels, learningRate=0.01, iterations=1000):
     return roomPrice, basePrice
 
 
-
-#plt.ylim(0,500)
-
 numberOfRooms = np.array([1,2,3,5,6,7])
 housePrices = np.array([155,197,244,356,407,448])
-x, y = linearRegression(numberOfRooms, housePrices)
+m, c = linearRegression(numberOfRooms, housePrices)
+#print(m, c)
 
-plt.plot(x,y,"r")
+labelVals = np.empty(len(numberOfRooms), dtype=float)
+for ind in range(len(numberOfRooms)): 
+    labelVal = m*(numberOfRooms[ind]) + c 
+    labelVals[ind] = labelVal
+    
+
+print(labelVals)
+
+plt.plot(numberOfRooms, labelVals, "r")
+plt.plot(numberOfRooms, housePrices, marker="*")
 plt.show()
