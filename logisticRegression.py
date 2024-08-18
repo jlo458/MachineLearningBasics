92ba0f69b86984cdb3c03c31b6057f06a368923b
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-08-18 19:28:32 +0100

    Create logisticRegression.py

---
+++ logisticRegression.py
@@ -0,0 +1,62 @@
+# This is my manual "logistic regression" algorithm
+
+import matplotlib
+matplotlib.use('TkAgg')  
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import utils
+
+features = np.array([[1,0],[0,2],[1,1],[1,2],[1,3],[2,2],[3,2],[2,3]])
+labels = np.array([0,0,0,0,1,1,1,1]) 
+
+#utils.plotPoints(features, labels)
+#plt.show()
+
+def sigmoid(x): 
+    return np.exp(x)/(np.exp(x)+1) # Same as 1/(1+np.exp(-x)) but this is better for small floating point numbers
+
+def score(weights, bias, features): 
+    return np.dot(weights, features) + bias 
+
+def prediction(weights, bias, features): 
+    return sigmoid(score(weights, bias, features))
+
+def logLoss(weights, bias, features, label): 
+    pred = prediction(weights, bias, features)
+    return -label*np.log(pred) - (1-label)*np.log(1-pred)
+
+def totalLogLoss(weights, bias, features, labels): 
+    total = 0 
+    for i in range(len(features)): 
+        total += logLoss(weights, bias, features[i], labels[i])
+
+    return total 
+
+def logisticTrick(weights, bias, features, label, learningRate = 0.01): 
+    pred = prediction(weights, bias, features)
+    for i in range(len(features)): 
+        weights[i] += (label-pred)*features[i]*learningRate 
+    bias += (label-pred)*learningRate 
+
+    return weights, bias 
+
+def logisticRegressionAlg(features, labels, epochs = 1000):
+    utils.plotPoints(features, labels)
+    weights = [1.0 for i in range(len(features[0]))]
+    bias = 0
+    #errors = []
+    for i in range(epochs): 
+        utils.drawLine(weights[0], weights[1], bias, color='grey', linewidth=0.1, linestyle='dotted')
+        j = random.randint(1, len(features)-1)
+        weights, bias = logisticTrick(weights, bias, features[j], labels[j])
+
+    utils.drawLine(weights[0], weights[1], bias)
+    plt.show()
+    return weights, bias
+
+print(logisticRegressionAlg(features, labels))
+
+
+
+
