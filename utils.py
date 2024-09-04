033ee4ba79dad6636a51f77daaaf45a2b957c139
jlo458 <130149735+jlo458@users.noreply.github.com>
2024-09-04 10:59:38 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+# Various utils for different bits of code
+
 import numpy as np
 import matplotlib
+matplotlib.use('TkAgg')
 from matplotlib import pyplot
 
 def plotPoints(features, labels): 
@@ -29,3 +32,20 @@ def plotPoints(features, labels):
 def drawLine(a,b,c, starting=0, ending=3, **kwargs): 
     x = np.linspace(starting, ending, 1000)
     pyplot.plot(x, (-a*x-c)/b, **kwargs)
+
+def plotModel(X, y, model, size_of_points=100):
+    X = np.array(X)
+    y = np.array(y)
+    plot_step = 0.2
+    x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, plot_step),
+                         np.arange(y_min, y_max, plot_step))
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    pyplot.contourf(xx, yy, Z, colors=['red', 'blue'], alpha=0.2, levels=range(-1,2))
+    pyplot.contour(xx, yy, Z,colors = 'k',linewidths = 1)
+    plotPoints(X, y)
+    pyplot.show()
+
+
