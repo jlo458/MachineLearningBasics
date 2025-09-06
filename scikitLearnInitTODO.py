dedbbb571154a9773b9b742ad6f138f2dea038c7
jlo458 <130149735+jlo458@users.noreply.github.com>
2025-09-06 14:00:04 +0100

    Update scikitLearnInitTODO.py

--- scikitLearnInitTODO.py
+++ scikitLearnInitTODO.py
@@ -12,9 +12,13 @@ import matplotlib.pyplot as plt
 
 #plt.use('TkAgg')
 
-linear_data = pd.read_csv("linear.csv") 
+'''linear_data = pd.read_csv("linear.csv") 
 features = np.array(linear_data[['x_1', 'x_2']])
-labels = np.array(linear_data['y']) 
+labels = np.array(linear_data['y']) '''
+
+poly_data = pd.read_csv("two_circles.csv")
+features = np.array(poly_data[['x_1','x_2']])
+labels = np.array(poly_data['y'])
 
 #utils.plotPoints(features, labels)
 
@@ -22,7 +26,7 @@ labels = np.array(linear_data['y'])
 svm_linear.fit(features, labels)
 print("Accuracy:", svm_linear.score(features, labels))'''
 
-# C = 0.01 
+'''# C = 0.01 - C value determines what to prioritise (accuracy or avg distance)
 svm_c_001 = SVC(kernel='linear', C=0.01)
 svm_c_001.fit(features, labels)
 print("C=0.01")
@@ -34,4 +38,19 @@ svm_c_100 = SVC(kernel='linear', C=100)
 svm_c_100.fit(features, labels)
 print("C=100")
 print("Accuracy:", svm_c_100.score(features, labels))
-utils.plotModel(features, labels, svm_c_100)
+utils.plotModel(features, labels, svm_c_100)'''
+
+'''# Polynomial - degree determines degree of polynomial created
+svm_p = SVC(kernel='poly', degree=4)
+svm_p.fit(features, labels)
+print("poly")
+print("Accuracy:", svm_p.score(features, labels))
+utils.plotModel(features, labels, svm_p)'''
+
+
+# RBF Kernel - change gamma for over/underfitting
+svm_gamma = SVC(kernel='rbf', gamma=4)
+svm_gamma.fit(features, labels)
+print("rbf")
+print("Accuracy:", svm_gamma.score(features, labels))
+utils.plotModel(features, labels, svm_gamma)
