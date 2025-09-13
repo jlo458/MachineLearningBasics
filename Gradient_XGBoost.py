d4e3446c3d455d84cd26615e73469ecf8ce2c853
jlo458 <130149735+jlo458@users.noreply.github.com>
2025-09-13 17:15:30 +0100

    Update Gradient_XGBoost.py

--- Gradient_XGBoost.py
+++ Gradient_XGBoost.py
@@ -23,3 +23,22 @@ for i in range(len(predictions)):
     print(f"Age {(i+1)*10} | Prediction: {predictions[i]}")
 
 utils.plot_regressor(features, labels, gradBoostRegressor)
+
+# XGBoost
+import xgboost
+from xgboost import XGBRegressor 
+
+features = np.array([[10],[20],[30],[40],[50],[60],[70],[80]])
+labels = np.array([7,5,7,1,2,1,5,4])
+
+xgboostRegressor = XGBRegressor(random_state=0,
+                             n_estimators=1000,
+                             max_depth=3,
+                             reg_lambda=3,
+                             min_split_loss=3,
+                             learning_rate=0.4)
+
+xgboostRegressor.fit(features, labels) 
+print(xgboostRegressor.score(features, labels))
+
+utils.plot_regressor(features, labels, xgboostRegressor)
